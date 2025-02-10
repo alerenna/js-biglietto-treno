cf35cdf296331a90fe472c8381881e8d396cd542
“alerenna” <alessandrorennafm@gmail.com>
2025-02-10 22:10:08 +0800

    Add function to calculate purcentage and give the price discounted

---
+++ main.js
@@ -0,0 +1,33 @@
+/* 
+Descrizione
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+
+- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+TOOLS
+
+- prompt x età e km
+- if
+ */
+
+
+const km = prompt('Quanti km vuoi percorrere?')
+const età = prompt('Quanti anni hai?')
+const costoKm = (0.21 * km)
+
+console.log(costoKm);
+
+if (età <= 18) {
+    alert('Il tuo biglietto costa € ' + (costoKm - (costoKm * 0.2)));
+} else if (età >= 65) {
+    alert('Il tuo biglietto costa € ' + (costoKm - (costoKm * 0.4)));
+} else {
+    alert('Il tuo biglietto costa € ' + costoKm);
+}
+
+
